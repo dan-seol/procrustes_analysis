7abb0b638d368d74f3b8af9254f2cf242aebe0fe
Dan Seol <yunheum.seol@gmail.com>
2020-05-31 23:49:52 -0400

    scaleFromPixelToMM

--- getTRSD.R
+++ getTRSD.R
@@ -57,6 +57,44 @@ getTRSD <- cmpfun(function(imagePath1, imagePath2) {
   scale <- procOPAResult$s
   translationMatrix <- matrix(colMeans(procOPAResult$Bhat - scale*learnerMarking %*% procOPAResult$R), ncol=2)
   colnames(translationMatrix) <- c("X", "Y")
+  rownames(translationMatrix) <- c("pixels")
+  
+  TRSD <- MarkingEvaluation(translation = translationMatrix,
+                            rotation = rotationAngleInDegree,
+                            scale = scale,
+                            dissimilarity = procOPAResult$rmsd)
+  
+  #access components by key "translation", "rotation", and "scale" finally "dissimilarity"
+  #using @, for example: TRSD@rotation
+  return(TRSD)
+})
+
+getTRSDInMilimiters <- cmpfun(function(imagePath1, imagePath2, MilimeterToPixel) {
+  #turn the images intro binary, where the marking is white and the background is black
+  thinnedImage1 <- readImage(imagePath1) %>% channel(., "gray") %>% `<`(., 0.5) %>% bwlabel(.) %>% thinImage
+  thinnedImage2 <- readImage(imagePath2) %>% channel(., "gray") %>% `<`(., 0.5) %>% bwlabel(.) %>% thinImage
+  
+  #take the 2d coordinates of the image pixels, only the part where it is surgical marking
+  curvePoints1 <- getCurvePoints(thinnedImage1)
+  curvePoints2 <- getCurvePoints(thinnedImage2)
+  dim1 <- dim(curvePoints1)[1]
+  dim2 <- dim(curvePoints2)[1]
+  minDim <- min(dim1, dim2)
+  #even out the number of points so that they can be comparable with procGPA
+  updatedCurvePoints1 <- DouglasPeuckerNbPoints(curvePoints1$x_coords, curvePoints1$y_coords, minDim)
+  updatedCurvePoints2 <- DouglasPeuckerNbPoints(curvePoints2$x_coords, curvePoints2$y_coords, minDim)
+  minDim <- min(dim(updatedCurvePoints1)[1], dim(updatedCurvePoints2)[1])
+  updatedCurvePoints1 <- DouglasPeuckerNbPoints(curvePoints1$x_coords, curvePoints1$y_coords, minDim)
+  updatedCurvePoints2 <- DouglasPeuckerNbPoints(curvePoints2$x_coords, curvePoints2$y_coords, minDim)
+  
+  learnerMarking <- as.matrix(updatedCurvePoints1) #to feed those in procGPA()
+  teacherMarking <- as.matrix(updatedCurvePoints2)
+  procOPAResult <- procOPAcompiled(teacherMarking, learnerMarking)
+  rotationAngleInDegree <- ( procOPAResult$R[1,1] %>% acos )*180/pi
+  scale <- procOPAResult$s
+  translationMatrix <- matrix(colMeans(procOPAResult$Bhat - scale*learnerMarking %*% procOPAResult$R), ncol=2)/MilimeterToPixel
+  colnames(translationMatrix) <- c("X", "Y")
+  rownames(translationMatrix) <- c("milimeters")
   TRSD <- MarkingEvaluation(translation = translationMatrix,
                             rotation = rotationAngleInDegree,
                             scale = scale,
